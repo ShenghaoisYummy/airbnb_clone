cd16d1761e466822bdaabce4c830dd3c9fcc617a
Your Name <you@example.com>
2025-02-03 15:35:14 +0800

    install shadcn components and folder

--- airbnb_clone/app/page.tsx
+++ airbnb_clone/app/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
 
 function HomePage() {
   return (
     <div>
       <h1 className='text-3xl'>Hoempage</h1>
+      <Button variant="outline" size="lg" className='capitalize m-8'>Click me</Button>
     </div>
   )
 }
