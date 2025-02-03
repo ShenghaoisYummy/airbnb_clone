2140bf2f78655ef81dd22340c63e98a54e17fa86
ShenghaoisYummy <hsupisces@hotmail.com>
2025-02-03 20:09:54 +0800

    Logo and NavSearch components

--- airbnb_clone/components/navbar/NavSearch.tsx
+++ airbnb_clone/components/navbar/NavSearch.tsx
@@ -1,10 +1,7 @@
+import { Input } from "@/components/ui/input";
 
 function NavSearch() {
-    return (
-        <div>
-            NavSearch
-        </div>
-    )
+    return <Input type="text" placeholder="find a property" className="max-w-xs" />
 }
 
 export default NavSearch
--- airbnb_clone/components/navbar/Navbar.tsx
+++ airbnb_clone/components/navbar/Navbar.tsx
@@ -6,13 +6,13 @@ import Logo from "./Logo";
 function Navbar() {
   return (
     <nav className="border-b">
-      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4">
+      <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         <Logo />
         <NavSearch />
-      </div>
-      <div className="flex items-center gap-4">
-        <LinksDropDown />
-        <DarkMode />
+        <div className="flex items-center gap-4">
+          <DarkMode />
+          <LinksDropDown />
+        </div>
       </div>
     </nav>
   );
