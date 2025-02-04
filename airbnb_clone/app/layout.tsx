38b624d16aefcb639e68434483a38a894412bf80
ShenghaoisYummy <hsupisces@hotmail.com>
2025-02-04 18:24:10 +0800

    clerk auth

--- airbnb_clone/app/layout.tsx
+++ airbnb_clone/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import Providers from './providers'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,14 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning className={inter.className}>
-        <Providers>
-          <Navbar />
-          <main className='container py-10'> </main>
-        </Providers>
-      </body>
-    </html >
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body suppressHydrationWarning className={inter.className}>
+          <Providers>
+            <Navbar />
+            <main className='container py-10'> </main>
+          </Providers>
+        </body>
+      </html >
+    </ClerkProvider>
   )
 }
