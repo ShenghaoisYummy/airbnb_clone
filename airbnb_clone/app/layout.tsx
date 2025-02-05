bbf8c3de9a41c5cde2b06417fb58eb510beb0a48
Your Name <you@example.com>
2025-02-05 17:53:45 +0800

    fix bugs

--- airbnb_clone/app/layout.tsx
+++ airbnb_clone/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import Providers from './providers'
-import './globals.css'
+import '@/app/global.css'
 import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
