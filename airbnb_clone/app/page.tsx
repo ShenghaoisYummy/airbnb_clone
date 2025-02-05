e07b50d17e57838e774cdd374d90388485471e7b
ShenghaoisYummy <hsupisces@hotmail.com>
2025-02-05 17:53:45 +0800

    fix bugs

--- airbnb_clone/app/page.tsx
+++ airbnb_clone/app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button'
 
 function HomePage() {
-  return (
-    <div>
-      <h1 className='text-3xl'>Hoempage</h1>
-      <Button variant="outline" size="lg" className='capitalize m-8'>Click me</Button>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className='text-3xl'>Hoempage</h1>
+            <Button variant="outline" size="lg" className='capitalize m-8'>Click me</Button>
+        </div>
+    )
 }
 
 export default HomePage
