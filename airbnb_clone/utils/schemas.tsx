7a84a48b7664ef24ce223f2390d59590ac2b50c1
Your Name <you@example.com>
2025-02-22 18:28:40 +1100

    ValidateWithZodSchema - Helper Function

--- airbnb_clone/utils/schemas.tsx
+++ airbnb_clone/utils/schemas.tsx
@@ -5,3 +5,14 @@ export const profileSchema = z.object({
     lastName: z.string().min(2, { message: 'Last name must be at least 2 characters' }),
     username: z.string().min(2, { message: 'Username must be at least 2 characters' }),
 });
+
+export function validateWithZodSchema<T>(schema: z.ZodSchema<T>, data: unknown): T {
+    // 3. validate form data
+    const result = schema.safeParse(data)
+    // 4. update the profile
+    if (!result.success) {
+        const errors = result.error.errors.map(error => error.message).join(', ')
+        throw new Error(errors)
+    }
+    return result.data
+}
