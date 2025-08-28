e962ff3840de44ea264c25b3912e62d49b9e213f
ShenghaoisYummy <hsupisces@Hotmail.com>
2025-08-28 22:46:28 +1000

    add Properties Container

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,10 +1,34 @@
-function PropertiesContainer({
+import { fetchProperties } from "@/utils/actions";
+import PropertiesList from "./PropertiesList";
+import EmptyList from "./EmptyList";
+import type { PropertyCardProps } from "@/utils/types";
+
+async function PropertiesContainer({
   category,
   search,
 }: {
   category?: string;
   search?: string;
 }) {
-  return <div>PropertiesContainer</div>;
+  const properties: PropertyCardProps[] = await fetchProperties({
+    category,
+    search,
+  });
+
+  if (properties.length === 0) {
+    return (
+      <EmptyList
+        heading="No results."
+        message="Try again with different search terms."
+        btnText="Clear Filters"
+      />
+    );
+  }
+
+  return (
+    <div>
+      <PropertiesList properties={properties} />
+    </div>
+  );
 }
 export default PropertiesContainer;
